7f4fb8e5c263b0863b515b477bae085708cbb6a7
Terry <tlin23@gmail.com>
2025-05-29 13:54:06 -0700

    Remove backend_legacy

--- backend_legacy/pipeline/processors/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import DocumentProcessor
-from .pdf_processor import PDFProcessor
-from .docx_processor import DOCXProcessor
-
-__all__ = ["DocumentProcessor", "PDFProcessor", "DOCXProcessor"]
